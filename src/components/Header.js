33dfb46585d80596fc4c857a7f072e902ea22c08
Onur Utku TOPALOĞLU <47296136+onurutku@users.noreply.github.com>
2020-08-17 15:15:19 +0300

    Ders 3.1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,21 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 const Header = ({ title }) => {
   return (
     <header>
       <nav className="container navbar navbar-expand-lg navbar-light justify-content-between">
-        <a className="navbar-brand" href="">EğitimBudur</a>
+        <a className="navbar-brand" href="#">EğitimBudur</a>
 
         <ul className="navbar-nav">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+          <li className="nav-item">
+            <Link to="/" activeClassName="active" className="nav-link">Ana Sayfa</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
-          </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown
-        </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a className="dropdown-item" href="#">Action</a>
-              <a className="dropdown-item" href="#">Another action</a>
-              <div className="dropdown-divider"></div>
-              <a className="dropdown-item" href="#">Something else here</a>
-            </div>
+            <Link to="/hakkimizda" activeClassName="active" className="nav-link">Hakkımızda</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            <Link to="/iletisim" activeClassName="active" className="nav-link">İletişim</Link>
           </li>
         </ul>
       </nav>
