48ddee657f4db77a1c58ded9fae8a596129693e5
Onur Utku TOPALOĞLU <47296136+onurutku@users.noreply.github.com>
2020-08-17 13:59:55 +0300

    Ders 2

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -3,15 +3,13 @@ import { IndexLink, Link } from 'react-router'
 import PropTypes from 'prop-types'
 import './PageLayout.scss'
 
+import Header from '../../components/Header'
+
 export const PageLayout = ({ children }) => (
-  <div className='container text-center'>
-    <h1>React Redux Starter Kit</h1>
-    <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName='page-layout__nav-item--active'>Counter</Link>
-    <div className='page-layout__viewport'>
-      {children}
-    </div>
+  <div className='container-fluid'>
+
+  <Header title="My Page" />
+
   </div>
 )
 PageLayout.propTypes = {
